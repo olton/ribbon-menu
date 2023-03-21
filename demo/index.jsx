3e92b66966b440503cea401731520c46765cff12
olton <serhii@pimenov.com.ua>
2023-03-21 14:52:14 +0200

    change position for dropdown to fixed

--- demo/index.jsx
+++ demo/index.jsx
@@ -39,125 +39,128 @@ const App = () => {
                         <span className={`caption-button btn-clo`}></span>
                     </div>
                 </div>
-
-                <RibbonMenu>
-                    <RibbonTab mode="static" label="Home"></RibbonTab>
-                    <RibbonTab label="File">
-                        <RibbonTabGroup title="Push Buttons">
-                            <RibbonButton caption="Mail" icon="mif-envelop" title="Write Mail" onClick={()=>{alert('Mail Button was Clicked!')}}/>
-                            <RibbonButton caption="Share" icon="mif-share"/>
-                            <RibbonDropdown>
-                                <RibbonButton caption="Apps" icon="mif-apps" title="Applications"/>
-                                <RibbonDropdownMenu>
-                                    <RibbonDropdownItem caption="Windows 10" onClick={(e)=>{console.log(e)}}/>
-                                    <RibbonDropdownItem caption="Windows 11"/>
-                                    <RibbonDropdownItem caption="Office 365"/>
-                                    <RibbonDropdownDivider/>
-                                    <RibbonButtonGroup active={2}>
-                                        <RibbonDropdownItem caption="Windows 10" onClick={(e)=>{console.log(e)}}/>
-                                        <RibbonDropdownItem caption="Windows 11"/>
-                                        <RibbonDropdownItem caption="Office 365"/>
-                                    </RibbonButtonGroup>
-                                    <RibbonDropdownDivider/>
-                                    <RibbonDropdownCheckItem checked caption="Windows 10" onClick={(e)=>{console.log(e)}}/>
-                                    <RibbonDropdownCheckItem checked caption="Windows 11"/>
-                                    <RibbonDropdownCheckItem checked caption="Office 365"/>
-                                    <RibbonDropdownDivider/>
-                                    <RibbonButtonGroup active={[2]} radio>
-                                        <RibbonDropdownItem caption="Windows 10"/>
-                                        <RibbonDropdownItem caption="Windows 11"/>
-                                        <RibbonDropdownItem caption="Office 365"/>
-                                    </RibbonButtonGroup>
-                                    <RibbonDropdownDivider/>
+                <div style={{overflow: "auto"}}>
+                    <div>
+                        <RibbonMenu>
+                            <RibbonTab mode="static" label="Home"></RibbonTab>
+                            <RibbonTab label="File">
+                                <RibbonTabGroup title="Push Buttons">
+                                    <RibbonButton caption="Mail" icon="mif-envelop" title="Write Mail" onClick={()=>{alert('Mail Button was Clicked!')}}/>
+                                    <RibbonButton caption="Share" icon="mif-share"/>
                                     <RibbonDropdown>
-                                        <RibbonDropdownItem caption="Apps"/>
+                                        <RibbonButton caption="Apps" icon="mif-apps" title="Applications"/>
+                                        <RibbonDropdownMenu>
+                                            <RibbonDropdownItem caption="Windows 10" onClick={(e)=>{console.log(e)}}/>
+                                            <RibbonDropdownItem caption="Windows 11"/>
+                                            <RibbonDropdownItem caption="Office 365"/>
+                                            <RibbonDropdownDivider/>
+                                            <RibbonButtonGroup active={2}>
+                                                <RibbonDropdownItem caption="Windows 10" onClick={(e)=>{console.log(e)}}/>
+                                                <RibbonDropdownItem caption="Windows 11"/>
+                                                <RibbonDropdownItem caption="Office 365"/>
+                                            </RibbonButtonGroup>
+                                            <RibbonDropdownDivider/>
+                                            <RibbonDropdownCheckItem checked caption="Windows 10" onClick={(e)=>{console.log(e)}}/>
+                                            <RibbonDropdownCheckItem checked caption="Windows 11"/>
+                                            <RibbonDropdownCheckItem checked caption="Office 365"/>
+                                            <RibbonDropdownDivider/>
+                                            <RibbonButtonGroup active={[2]} radio>
+                                                <RibbonDropdownItem caption="Windows 10"/>
+                                                <RibbonDropdownItem caption="Windows 11"/>
+                                                <RibbonDropdownItem caption="Office 365"/>
+                                            </RibbonButtonGroup>
+                                            <RibbonDropdownDivider/>
+                                            <RibbonDropdown>
+                                                <RibbonDropdownItem caption="Apps"/>
+                                                <RibbonDropdownMenu>
+                                                    <RibbonDropdownItem caption="Windows 10"/>
+                                                    <RibbonDropdownItem caption="Windows 11"/>
+                                                    <RibbonDropdownItem caption="Office 365"/>
+                                                </RibbonDropdownMenu>
+                                            </RibbonDropdown>
+                                        </RibbonDropdownMenu>
+                                    </RibbonDropdown>
+                                </RibbonTabGroup>
+
+                                <RibbonTabGroup title="Icon and Tool buttons">
+                                    <RibbonTabSubGroup style={{width: '70px'}}>
+                                        <RibbonIconButton caption="Mail" icon="mif-envelop"/>
+                                        <RibbonIconButton caption="Share" icon="mif-share"/>
+                                        <RibbonDropdown>
+                                            <RibbonIconButton caption="Rocket" icon="mif-rocket"/>
+                                            <RibbonDropdownMenu>
+                                                <RibbonDropdownItem caption="Windows 10"/>
+                                                <RibbonDropdownItem caption="Windows 11"/>
+                                                <RibbonDropdownItem caption="Office 365"/>
+                                            </RibbonDropdownMenu>
+                                        </RibbonDropdown>
+                                    </RibbonTabSubGroup>
+
+                                    <RibbonTabDivider/>
+
+                                    <RibbonTabSubGroup style={{display: "flex", flexFlow: "row wrap", width: "74px"}}>
+                                        <RibbonToolButton caption="Mail" icon="mif-envelop"/>
+                                        <RibbonToolButton caption="Share" icon="mif-share"/>
+                                        <RibbonToolButton caption="Rocket" icon="mif-rocket"/>
+                                        <RibbonToolButton caption="Settings" icon="mif-cogs"/>
+                                        <RibbonToolButton caption="Bell" icon="mif-bell"/>
+                                        <RibbonToolButton caption="Alarm" icon="mif-alarm"/>
+                                        <RibbonDropdown>
+                                            <RibbonToolButton caption="Apps" icon="mif-apps"/>
+                                            <RibbonDropdownMenu>
+                                                <RibbonDropdownItem caption="Windows 10"/>
+                                                <RibbonDropdownItem caption="Windows 11"/>
+                                                <RibbonDropdownItem caption="Office 365"/>
+                                            </RibbonDropdownMenu>
+                                        </RibbonDropdown>
+                                    </RibbonTabSubGroup>
+                                </RibbonTabGroup>
+
+                                <RibbonTabGroup title="Split Button">
+                                    <RibbonSplitButton caption="Options" icon="mif-cog">
                                         <RibbonDropdownMenu>
                                             <RibbonDropdownItem caption="Windows 10"/>
                                             <RibbonDropdownItem caption="Windows 11"/>
                                             <RibbonDropdownItem caption="Office 365"/>
+                                            <RibbonDropdownDivider/>
+                                            <RibbonDropdownCheckItem checked caption="Windows 10"/>
+                                            <RibbonDropdownCheckItem checked caption="Windows 11"/>
+                                            <RibbonDropdownCheckItem checked caption="Office 365"/>
+                                            <RibbonDropdownDivider/>
                                         </RibbonDropdownMenu>
-                                    </RibbonDropdown>
-                                </RibbonDropdownMenu>
-                            </RibbonDropdown>
-                        </RibbonTabGroup>
+                                    </RibbonSplitButton>
+                                </RibbonTabGroup>
 
-                        <RibbonTabGroup title="Icon and Tool buttons">
-                            <RibbonTabSubGroup style={{width: '70px'}}>
-                                <RibbonIconButton caption="Mail" icon="mif-envelop"/>
-                                <RibbonIconButton caption="Share" icon="mif-share"/>
-                                <RibbonDropdown>
-                                    <RibbonIconButton caption="Rocket" icon="mif-rocket"/>
-                                    <RibbonDropdownMenu>
-                                        <RibbonDropdownItem caption="Windows 10"/>
-                                        <RibbonDropdownItem caption="Windows 11"/>
-                                        <RibbonDropdownItem caption="Office 365"/>
-                                    </RibbonDropdownMenu>
-                                </RibbonDropdown>
-                            </RibbonTabSubGroup>
+                                <RibbonTabGroup title="Button Groups">
 
-                            <RibbonTabDivider/>
+                                    <RibbonButtonGroup radio  style={{width: "200px", maxHeight: "88px", }}>
+                                        <RibbonIconButton caption="List" icon="mif-list"/>
+                                        <RibbonIconButton caption="Gear" icon="mif-cog"/>
+                                        <RibbonIconButton caption="Barcode" icon="mif-barcode"/>
+                                        <RibbonIconButton caption="Bell" icon="mif-bell"/>
+                                        <RibbonIconButton caption="Cast" icon="mif-cast"/>
+                                        <RibbonIconButton caption="Calculator" icon="mif-calculator2"/>
+                                    </RibbonButtonGroup>
 
-                            <RibbonTabSubGroup style={{display: "flex", flexFlow: "row wrap", width: "74px"}}>
-                                <RibbonToolButton caption="Mail" icon="mif-envelop"/>
-                                <RibbonToolButton caption="Share" icon="mif-share"/>
-                                <RibbonToolButton caption="Rocket" icon="mif-rocket"/>
-                                <RibbonToolButton caption="Settings" icon="mif-cogs"/>
-                                <RibbonToolButton caption="Bell" icon="mif-bell"/>
-                                <RibbonToolButton caption="Alarm" icon="mif-alarm"/>
-                                <RibbonDropdown>
-                                    <RibbonToolButton caption="Apps" icon="mif-apps"/>
-                                    <RibbonDropdownMenu>
-                                        <RibbonDropdownItem caption="Windows 10"/>
-                                        <RibbonDropdownItem caption="Windows 11"/>
-                                        <RibbonDropdownItem caption="Office 365"/>
-                                    </RibbonDropdownMenu>
-                                </RibbonDropdown>
-                            </RibbonTabSubGroup>
-                        </RibbonTabGroup>
+                                    <RibbonButtonGroup style={{width: "100px"}}>
+                                        <RibbonIconButton caption="Italic" icon="mif-italic"/>
+                                        <RibbonIconButton caption="Bold" icon="mif-bold"/>
+                                        <RibbonIconButton caption="Underline" icon="mif-underline"/>
+                                    </RibbonButtonGroup>
 
-                        <RibbonTabGroup title="Split Button">
-                            <RibbonSplitButton caption="Options" icon="mif-cog">
-                                <RibbonDropdownMenu>
-                                    <RibbonDropdownItem caption="Windows 10"/>
-                                    <RibbonDropdownItem caption="Windows 11"/>
-                                    <RibbonDropdownItem caption="Office 365"/>
-                                    <RibbonDropdownDivider/>
-                                    <RibbonDropdownCheckItem checked caption="Windows 10"/>
-                                    <RibbonDropdownCheckItem checked caption="Windows 11"/>
-                                    <RibbonDropdownCheckItem checked caption="Office 365"/>
-                                    <RibbonDropdownDivider/>
-                                </RibbonDropdownMenu>
-                            </RibbonSplitButton>
-                        </RibbonTabGroup>
+                                    <RibbonButtonGroup active={[1, 3]} style={{width: "74px", display: "flex", flexFlow: "row", justifyContent: "center"}}>
+                                        <RibbonToolButton caption="Italic" icon="mif-italic"/>
+                                        <RibbonToolButton caption="Bold" icon="mif-bold"/>
+                                        <RibbonToolButton caption="Underline" icon="mif-underline"/>
+                                    </RibbonButtonGroup>
 
-                        <RibbonTabGroup title="Button Groups">
-
-                            <RibbonButtonGroup radio  style={{width: "200px", maxHeight: "88px", }}>
-                                <RibbonIconButton caption="List" icon="mif-list"/>
-                                <RibbonIconButton caption="Gear" icon="mif-cog"/>
-                                <RibbonIconButton caption="Barcode" icon="mif-barcode"/>
-                                <RibbonIconButton caption="Bell" icon="mif-bell"/>
-                                <RibbonIconButton caption="Cast" icon="mif-cast"/>
-                                <RibbonIconButton caption="Calculator" icon="mif-calculator2"/>
-                            </RibbonButtonGroup>
-
-                            <RibbonButtonGroup style={{width: "100px"}}>
-                                <RibbonIconButton caption="Italic" icon="mif-italic"/>
-                                <RibbonIconButton caption="Bold" icon="mif-bold"/>
-                                <RibbonIconButton caption="Underline" icon="mif-underline"/>
-                            </RibbonButtonGroup>
-
-                            <RibbonButtonGroup active={[1, 3]} style={{width: "74px", display: "flex", flexFlow: "row", justifyContent: "center"}}>
-                                <RibbonToolButton caption="Italic" icon="mif-italic"/>
-                                <RibbonToolButton caption="Bold" icon="mif-bold"/>
-                                <RibbonToolButton caption="Underline" icon="mif-underline"/>
-                            </RibbonButtonGroup>
-
-                        </RibbonTabGroup>
-                    </RibbonTab>
-                    <RibbonTab label="Edit"></RibbonTab>
-                    <RibbonTab label="View"></RibbonTab>
-                </RibbonMenu>
+                                </RibbonTabGroup>
+                            </RibbonTab>
+                            <RibbonTab label="Edit"></RibbonTab>
+                            <RibbonTab label="View"></RibbonTab>
+                        </RibbonMenu>
+                    </div>
+                </div>
             </div>
         </section>
     )
